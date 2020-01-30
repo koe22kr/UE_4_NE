31a05264246c3f24d2af2037fe304f93c5ce839d
Sophia <sophiajh24@gmail.com>
2020-01-30 12:44:49 +0900

    master commit

---
+++ Source/Team_NE/Team_NE.cpp
@@ -0,0 +1,6 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#include "Team_NE.h"
+#include "Modules/ModuleManager.h"
+
+IMPLEMENT_PRIMARY_GAME_MODULE( FDefaultGameModuleImpl, Team_NE, "Team_NE" );
---
+++ Source/Team_NE/Team_NEGameModeBase.h
@@ -0,0 +1,17 @@
+// Fill out your copyright notice in the Description page of Project Settings.
+
+#pragma once
+
+#include "CoreMinimal.h"
+#include "GameFramework/GameModeBase.h"
+#include "Team_NEGameModeBase.generated.h"
+
+/**
+ * 
+ */
+UCLASS()
+class TEAM_NE_API ATeam_NEGameModeBase : public AGameModeBase
+{
+	GENERATED_BODY()
+	
+};
